339a8e7aa4873913ddd6aac1b228767a68c5e695
tauri_s <tauristclaire@gmail.com>
2022-08-04 18:15:05 -0700

    Started function to find specific item

--- js/script.js
+++ js/script.js
@@ -18,3 +18,16 @@ var groceries = [
   "olive oil",
   "mustard"
 ];
+
+//Find a particular item and remove it
+//First use indexOf to act on item, make it a variable bc you will use it in conditionals to decide what to do with it
+var removeItems = function (array, item) {
+  var index = array.indexOf(item);
+
+  if (index === -1) {
+    console.log(`Sorry, no such item in this array.`);
+  } else {
+    array.splice(index, 1);
+    console.log(`Removing ${item}`);
+  }
+};
